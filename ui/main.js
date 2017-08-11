b2668b6729f6d61fade9f41b0a814ee9c401d992
Naveenkumar322 <30624245+Naveenkumar322@users.noreply.github.com>
2017-08-11 14:50:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,15 @@ element.innerHTML = 'New value';
 
 //Move the image
 var img = document.getElementById('madi');
+var marginleft=0;
+function moveRight() {
+    marginleft = marginleft +10;
+    img.style.marginleft = marginleft + 'px';
+}
+
+
+
 img.onclick = function () {
-    img.style.marginleft='100px';
+    var interval = setinterval(moveight,100);
+    
 };
