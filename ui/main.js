3afad0494b9d79bc072400aeb5d6b6b40e120ab9
Naveenkumar322 <30624245+Naveenkumar322@users.noreply.github.com>
2017-08-11 12:18:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,16 @@
 console.log('Loaded!');
 //change the text of the main-text.div
-var element=document.getElementbyId ('main-text'
+var element= document.getElementbyId ('main-text'
 );
 
 element.innerHTML='New Value';
 //Move the Image
 var img=document.getelementbyid('madi');
+var marginleft=0;
+function moveRight (){
+    marginleft-marginleft + 10;
+    img.style.marginleft=marginleft +'px';
+}
+img.onclick=function() {
+    var interval=setinterval(moveright,50);
+};
